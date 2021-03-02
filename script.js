d2551abd4a2bda8df3a60bbe639e4f688ec7eecb
Dan <danieljamieprice@gmail.com>
2021-03-02 22:03:06 +0000

    add mouseover and click event listeners + add reset function

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const container = document.getElementById("container");
 let divSquare = document.createElement("div");
 divSquare.classList = "div-squares";
+const squares = document.querySelectorAll(".div-squares");
+
 function fillSquares(amount) {
     for (let i = 0; i < amount*amount; i++)
     container.append(divSquare.cloneNode());
@@ -9,7 +11,40 @@ function fillSquares(amount) {
 function changeDimensions(dimension) {
     container.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${dimension}, 1fr)`;
+    fillSquares(dimension);
 }
-changeDimensions(32);
-console.log(container.style.gridTemplateColumns);
-fillSquares(32);
+function reset() {
+    squares.forEach(square => {
+        square.style.backgroundColor = "white";   
+    });
+}
+function addGlobalEventListener(type, selector, callback) {
+    document.addEventListener(type, e => {
+        if (e.target.matches(selector))
+            callback(e);
+    })
+}
+
+
+
+addGlobalEventListener("mouseover", ".div-squares", (e) => {
+    e.target.style.backgroundColor = "black";
+});
+addGlobalEventListener("click", "#dimensions-btn", (e) => {
+    let input = prompt("Enter a number between 1-64");
+    let valid = false;
+    while (!valid) {
+        if (isNaN(input) || input < 1 || input > 64)
+            input = prompt("Input not valid, enter a number between 1-64");
+        else
+            valid = true;
+    }
+    changeDimensions(input);
+});
+addGlobalEventListener("click", "#reset-btn", (e) => {
+    console.log(e.target);
+    reset();
+});
+
+
+changeDimensions(16);
