d56e7b63b750bf0eef0c760ff26955f08eb881f2
dan <danieljamieprice@gmail.com>
2021-03-03 00:36:20 +0000

    add removeSquares function, add randomColor function, fix reset function

--- script.js
+++ script.js
@@ -2,21 +2,32 @@
 const container = document.getElementById("container");
 let divSquare = document.createElement("div");
 divSquare.classList = "div-squares";
-const squares = document.querySelectorAll(".div-squares");
+
 
 function fillSquares(amount) {
     for (let i = 0; i < amount*amount; i++)
     container.append(divSquare.cloneNode());
 }
+function removeSquares() {
+    while (container.hasChildNodes()) {  
+        container.removeChild(container.firstChild);
+    }
+}
 function changeDimensions(dimension) {
+    removeSquares();
     container.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${dimension}, 1fr)`;
     fillSquares(dimension);
 }
 function reset() {
+    const squares = document.querySelectorAll(".div-squares");
     squares.forEach(square => {
-        square.style.backgroundColor = "white";   
+        square.style.backgroundColor = "";
     });
+}
+function randomColor() {
+    let o = Math.round, r = Math.random, s = 255;
+    return "rgba(" + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ")";
 }
 function addGlobalEventListener(type, selector, callback) {
     document.addEventListener(type, e => {
@@ -28,19 +39,25 @@ function addGlobalEventListener(type, selector, callback) {
 
 
 addGlobalEventListener("mouseover", ".div-squares", (e) => {
-    e.target.style.backgroundColor = "black";
+    if (document.getElementById("rgb-btn").hasAttribute("rgb-active"))
+        e.target.style.backgroundColor = randomColor();
+    else
+        e.target.style.backgroundColor = "black";
 });
 addGlobalEventListener("click", "#dimensions-btn", (e) => {
     let input = prompt("Enter a number between 1-64");
     let valid = false;
     while (!valid) {
-        if (isNaN(input) || input < 1 || input > 64)
+        if (isNaN(input) || input < 1 || input > 100)
             input = prompt("Input not valid, enter a number between 1-64");
         else
             valid = true;
     }
     changeDimensions(input);
 });
+addGlobalEventListener("click", "#rgb-btn", (e) => {
+    e.target.toggleAttribute("rgb-active");
+})
 addGlobalEventListener("click", "#reset-btn", (e) => {
     console.log(e.target);
     reset();
